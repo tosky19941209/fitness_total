2da4f8057faf592c45a1d0250575b9866377e619
felixeliass1994 <felixeliass1994@gmail.com>
2024-04-07 03:51:59 +0900

    'completed_step1'

--- fitness-dashboard/src/service/axios.js
+++ fitness-dashboard/src/service/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 const port = 8080
 const api = axios.create({
-    baseURL:`http://localhost:${port}/api`
+    // baseURL:`http://localhost:${port}/api`
     // baseURL:`https://node-backend-fitness.vercel.app/api`
-    // baseURL:`http://77.37.86.133:8080/api`
+    baseURL:`http://77.37.86.133:8080/api`
 })
 
 export default api;
