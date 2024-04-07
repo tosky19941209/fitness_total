bf429d32a5900f1f77c44c862087847bda936a77
felixeliass1994 <felixeliass1994@gmail.com>
2024-04-07 18:52:30 +0900

    'http-https

--- new_backend/server.js
+++ new_backend/server.js
@@ -4,9 +4,20 @@ var config = require('./env/config')
 var app = express()
 mongoose()
 
+var https = require('https')
 // app.listen(config.port, '77.37.86.133', () => {
 //     console.log(`Server is running on port ${config.port}`)
 // })
-app.listen(config.port, () => {
-    console.log(`Server is running on port ${config.port}`)
-})
+
+// app.listen(config.port, '192.168.142.113', () => {
+//     console.log(`Server is running on port ${config.port}`)
+// })
+
+const server = https.Server(app)
+
+server.listen( config.port, '77.37.86.133', () => {
+    console.log(
+        `Server is running on port ${config.port}`
+    )
+} )
+
